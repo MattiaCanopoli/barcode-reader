f99c270b57659a7e70e71be01bc98eacfea1a9f8
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-11-09 11:49:52 +0100

    feat: barcodeTest method
    a method to test if provided barcode exist within the provided List<String>

--- src/main/java/com/canopoli/barcode/reader/io/BarcodeTest.java
+++ src/main/java/com/canopoli/barcode/reader/io/BarcodeTest.java
@@ -1,5 +1,16 @@
 package com.canopoli.barcode.reader.io;
 
+import java.util.List;
+
 public class BarcodeTest {
+	
+	public static boolean barcodeTest(String barcode, List<String> barcodes) {
+				
+		if (barcodes.contains(barcode)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
