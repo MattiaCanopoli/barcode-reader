237154c8658a0d142e346434046e49ebf30c4c6e
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-11-12 10:58:12 +0100

    cleaned code

--- src/main/java/com/canopoli/barcode/reader/io/FileOutput.java
+++ src/main/java/com/canopoli/barcode/reader/io/FileOutput.java
@@ -10,32 +10,32 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileOutput {
-	
+
 	public static void saveFile (List<String> barcodes) throws IOException {
-		
+
 		JFileChooser dirChooser = new JFileChooser();
-		//dirChooser .setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); 
+		//dirChooser .setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		dirChooser.setAcceptAllFileFilterUsed(false);
 		FileNameExtensionFilter filter = new FileNameExtensionFilter(".txt","Text files");
 
 		dirChooser.setFileFilter(filter);
 		dirChooser.showSaveDialog(dirChooser);
-		
+
 		File file = dirChooser.getSelectedFile();
-		
+
 		FileWriter fileWriter = new FileWriter(file);
-		
+
 		BufferedWriter writer = new BufferedWriter(fileWriter);
 		for (String barcode : barcodes) {
 			writer.write(barcode + "\n");
 		}
-		
+
 		writer.close();
 		fileWriter.close();
-		
+
 	}
-	
-	
+
+
 //	public static void saveFile (String[] barcodes, String outputFile ) throws IOException {
 //		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
 //		for (String barcode : barcodes) {
--- src/main/java/com/canopoli/barcode/reader/utility/LabelMsgUtils.java
+++ src/main/java/com/canopoli/barcode/reader/utility/LabelMsgUtils.java
@@ -3,19 +3,19 @@ package com.canopoli.barcode.reader.utility;
 import javax.swing.JLabel;
 
 public class LabelMsgUtils {
-	
+
 	public static void labelSuccessGreen (JLabel label, String message) {
 		label.setForeground(new java.awt.Color(0, 222, 0));
 		label.setText(message);
 		label.setVisible(true);
 	}
-	
+
 	public static void labelFailRed (JLabel label,String message) {
 		label.setForeground(new java.awt.Color(255, 0, 0));
 		label.setText(message);
 		label.setVisible(true);
 	}
-	
+
 	public static void labelStandardBlue (JLabel label,String message) {
 		label.setForeground(new java.awt.Color(0, 0, 204));
 		label.setVisible(true);
